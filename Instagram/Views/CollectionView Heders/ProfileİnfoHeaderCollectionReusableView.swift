b95b5d7365872247d5dada7cd83af4eacc59ca86
ambercatalbas <ambercatalbas@gmail.com>
2021-04-30 19:21:58 +0300

    add frames in headercollectionreusableview

--- Instagram/Views/CollectionView Heders/ProfileİnfoHeaderCollectionReusableView.swift
+++ Instagram/Views/CollectionView Heders/ProfileİnfoHeaderCollectionReusableView.swift
@@ -9,13 +9,109 @@ import UIKit
 
 class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
        static let identifier = "ProfileInfoHeaderCollectionReusableView"
+    
+    private let profilePhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    private let postsButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Posts", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    private let followingButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Following", for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    private let followersButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        button.setTitle("Followers", for: .normal)
+        return button
+    }()
+    private let editProfileButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Edit Your Profile", for: .normal)
+        button.backgroundColor = .secondarySystemBackground
+        return button
+    }()
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    private let bioLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    //Mark: - init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemBlue
+        addSubviews()
+        backgroundColor = .systemBackground
         clipsToBounds = true
+    }
+    private func addSubviews() {
+        addSubview(profilePhotoImageView)
+        addSubview(followersButton)
+        addSubview(followingButton)
+        addSubview(postsButton)
+        addSubview(bioLabel)
+        addSubview(nameLabel)
+        addSubview(editProfileButton)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let profilePhotoSize = width/4
+        profilePhotoImageView.frame = CGRect(
+            x: 5,
+            y: 5,
+            width: profilePhotoSize,
+            height: profilePhotoSize
+        ).integral
+        
+        profilePhotoImageView.layer.cornerRadius = profilePhotoSize/2.0
+        
+        let buttonHeight = profilePhotoSize/2
+        let countButtonWidth = (width-10-profilePhotoSize)/3
+        postsButton.frame = CGRect(
+            x: profilePhotoImageView.right,
+            y: 5,
+            width: countButtonWidth,
+            height: buttonHeight
+        ).integral
+        followersButton.frame = CGRect(
+            x: postsButton.right,
+            y: 5,
+            width: countButtonWidth,
+            height: buttonHeight
+        ).integral
+        followingButton.frame = CGRect(
+            x: followersButton.right,
+            y: 5,
+            width: countButtonWidth,
+            height: buttonHeight
+        ).integral
+        editProfileButton.frame = CGRect(
+            x: profilePhotoImageView.right,
+            y: 5 + buttonHeight,
+            width: countButtonWidth*3,
+            height: buttonHeight
+        ).integral
+    }
+    
 }
