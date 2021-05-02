4bf9d31f18f612b24e200ae6418a4d2232ef0ba6
ambercatalbas <ambercatalbas@gmail.com>
2021-05-02 23:16:36 +0300

    2

--- Instagram/Controllers/Other/General/ListViewController.swift
+++ Instagram/Controllers/Other/General/ListViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    private let data: [String]
+    
+    init(data: [String]) {
+        self.data = data
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
