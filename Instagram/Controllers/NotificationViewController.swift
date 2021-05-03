9fb7391bb3e14d67e201ee6275ca0b41e22c608b
ambercatalbas <ambercatalbas@gmail.com>
2021-05-04 00:47:28 +0300

    add NotificationCells(follow and like)

--- Instagram/Controllers/NotificationViewController.swift
+++ Instagram/Controllers/NotificationViewController.swift
@@ -13,15 +13,29 @@ class NotificationViewController: UIViewController, UITableViewDelegate, UITable
 
     private let tableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(NotificationlLikeEventTableViewCellTableViewCell.self, forCellReuseIdentifier: NotificationlLikeEventTableViewCellTableViewCell.identifier)
+        tableView.register(NotificationFollowEventTableViewCell.self, forCellReuseIdentifier: NotificationFollowEventTableViewCell.identifier)
+        tableView.isHidden = false
         return tableView
     }()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.tintColor = .label
+        return spinner
+    }()
+    private lazy var noNotificationsView = NoNotificationsView()
+    
+    ///MARK: Lifesycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Notifications"
+        navigationItem.title = "Notifications"
         view.backgroundColor = .systemBackground
+        view.addSubview(spinner)
+        //spinner.startAnimating()
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
@@ -30,8 +44,17 @@ class NotificationViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
+        spinner.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        spinner.center = view.center
+        
+     }
+    private func layoutNotificationsView() {
+        tableView.isHidden = true
+        view.addSubview(tableView)
+        noNotificationsView.frame = CGRect(x: 0, y: 0, width: view.width/2, height: view.width/2)
+        noNotificationsView.center = view.center
+    
     }
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
